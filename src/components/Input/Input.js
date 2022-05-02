155f11674a36a1f3fed37edf603bc6bbb126fad2
nmarupaka <marupakanavya@gmail.com>
2022-05-02 10:20:44 -0400

    update

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,6 +4,7 @@ import './Input.css';
 function Input(props) {
   const { size = 'medium', ...rest } = props;
   return <input className={`input ${size}`} {...rest}></input>;
+      // "chromatic": "npx chromatic --project-token=fef05b9caa9c"
 }
 
 export default Input;
