5aa4041a7bf997720222a4f66f28de4b6f8ba739
nmarupaka <marupakanavya@gmail.com>
2022-05-02 10:08:09 -0400

    update

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -15,13 +15,13 @@ export default {
 
 export const Primary = () => (
   <Button onClick={action('Click handler')} variant='primary' placeholder='primary'>
-    PrimaryOne
+    Primary-One
   </Button>
 );
 
 export const Secondary = () => (
   <Button {...actions('onClick', 'onMouseOver')} variant='secondary' placeholder='secondary'>
-    SecondaryOne
+    Secondary-One
   </Button>
 );
 
